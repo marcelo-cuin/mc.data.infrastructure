4530fbab43241b03ca0cc07fc8002da2fd8094c9
Marcelo Cuin <marcelo.cuin@hotmail.com>
2019-09-20 00:49:56 -0300

    Melhorias na estrutura da UnitOfWork

--- UnitOfWork.cs
+++ UnitOfWork.cs
@@ -15,31 +15,32 @@ namespace mc.data.infrastructure
         private const string DATABASE_ORACLE = "ORACLE";
 
         public DbContext Context { get => _dbContext; }
-        public virtual string ConnectionStringName { get => throw new NotImplementedException(); }
+        public virtual string ApplicationContext { get => throw new NotImplementedException(); }
+        public virtual string ConnectionName { get => throw new NotImplementedException(); }
 
-        public UnitOfWork(IConfiguration configuration)
+        public UnitOfWork(IConfiguration appConfiguration)
         {
             DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
 
-            string environment = configuration.GetValue<String>("environment");
+            string environment = appConfiguration.GetValue<String>("environment");
 
-            Configuration appConfig = new Configuration();
-            configuration.GetSection(environment).GetSection(this.ConnectionStringName).Bind(appConfig);
+            ConfigurationDatabase database = new ConfigurationDatabase();
 
-            string database = appConfig.Database.type;
-            string connectionString = appConfig.Database.connectionString;
+            appConfiguration.GetSection(environment)
+                            .GetSection(this.ApplicationContext)
+                            .Bind(this.ConnectionName, database);
 
-            switch (database)
+            switch (database.type)
             {
                 case DATABASE_MYSQL:
-                    builder.UseMySql(connectionString, mysql =>
+                    builder.UseMySQL(database.connectionString, mysql =>
                     {
                         mysql.CommandTimeout(60);
                     });
                     break;
 
                 case DATABASE_MSSQL:
-                    builder.UseSqlServer(connectionString, mssql =>
+                    builder.UseSqlServer(database.connectionString, mssql =>
                     {
                         mssql.CommandTimeout(60);
                     });
